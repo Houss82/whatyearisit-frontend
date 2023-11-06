bfb42a3b575580fba4a7c8b422e6e8cee41686ce
Houss82 <houssem1982mansour@gmail.com>
2023-11-06 15:56:55 +0100

    deploytovercel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-pi-drab.vercel.app/')
+fetch('https://whatyearisit-backend-pi-drab.vercel.app/year')
    .then(response => response.json())
    .then(data => {
          document.querySelector("#year").textContent = data.year });
